11a9f3f3e791ed0e17ad71830229214433400461
Yogita Goyal <yogitagoel2006@gmail.com>
2025-03-21 15:48:34 +0530

    Update BoxV2.sol

---
+++ src/BoxV2.sol
@@ -0,0 +1,34 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {UUPSUpgradeable} from "../lib/openzeppelin-contracts-upgradeable/contracts/proxy/utils/UUPSUpgradeable.sol";
+import {Initializable} from "../lib/openzeppelin-contracts-upgradeable/contracts/proxy/utils/Initializable.sol";
+import {OwnableUpgradeable} from "../lib/openzeppelin-contracts-upgradeable/contracts/access/OwnableUpgradeable.sol";
+
+contract BoxV2 is Initializable, OwnableUpgradeable, UUPSUpgradeable {
+    uint256 internal number;
+
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize() public initializer {
+        __Ownable_init(msg.sender);
+        __UUPSUpgradeable_init(); 
+    }
+
+    function setNumber(uint256 _number) public {
+        number = _number;
+    }
+
+    function getNumber() public view returns (uint256) {
+        return number;
+    }
+
+    function version() public pure returns (uint256) {
+        return 2;
+    }
+
+    function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
+}
